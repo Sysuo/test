90a6533137a2f451c385e3f2ca7ddf5355528cbc
a_kurosawa <a_kurosawa@RJC045.r-jc.local>
2013-05-30 17:10:37 +0900

    テストコミットされてなかった

--- test/test/Test.py
+++ test/test/Test.py
@@ -4,8 +4,12 @@ Created on 2013/05/30
 @author: a_kurosawa
 '''
 
-print("hello world")
+print("hello" + " " + "world")
 
+for i in range(10):
+    print("num:" +str(i))
+
+# これなんだ？
 if __name__ == '__main__':
-	pass
+    pass
 
